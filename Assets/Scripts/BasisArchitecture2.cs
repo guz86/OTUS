9e575e1bbddae7f8b53dcd89d25860ae9bc224dd
Pavel Gordienko <guzmo6@rambler.ru>
2022-11-11 04:35:33 +0300

    SRP

--- Assets/Scripts/BasisArchitecture2.cs
+++ Assets/Scripts/BasisArchitecture2.cs
@@ -4,179 +4,180 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-
-public class BasisArchitecture2 : MonoBehaviour
+namespace DefaultNamespace
 {
-}
+    public class BasisArchitecture2 : MonoBehaviour
+    {
+    }
 
 // DryExample
 
-public class WarriorMelee : MonoBehaviour
-{
-    private float _attackRange = 3f;
-
-    public bool IsEnemyInRange(Vector3 enemyPosition)
+    public class WarriorMelee : MonoBehaviour
     {
-        // .magnitude дистанция между векторами
-        return (transform.position - enemyPosition).magnitude <= _attackRange;
-    }
-}
+        private float _attackRange = 3f;
 
-public class WarriorRanged : MonoBehaviour
-{
-    private float _attackRange = 3f;
-
-    public bool IsEnemyInRange(Vector3 enemyPosition)
-    {
-        // .magnitude дистанция между векторами
-        return (transform.position - enemyPosition).magnitude <= _attackRange &&
-               CanSeeTarget(enemyPosition);
+        public bool IsEnemyInRange(Vector3 enemyPosition)
+        {
+            // .magnitude дистанция между векторами
+            return (transform.position - enemyPosition).magnitude <= _attackRange;
+        }
     }
 
-    // цель в зоне прямой видимости, не за камнем        
-    private bool CanSeeTarget(Vector3 target)
+    public class WarriorRanged : MonoBehaviour
     {
-        return true;
+        private float _attackRange = 3f;
+
+        public bool IsEnemyInRange(Vector3 enemyPosition)
+        {
+            // .magnitude дистанция между векторами
+            return (transform.position - enemyPosition).magnitude <= _attackRange &&
+                   CanSeeTarget(enemyPosition);
+        }
+
+        // цель в зоне прямой видимости, не за камнем        
+        private bool CanSeeTarget(Vector3 target)
+        {
+            return true;
+        }
     }
-}
 
 // варианты избавления от повторения кода
 
 // базовый класс // если не будет использоваться то будет абстрактным
 
-public abstract class Warrior : MonoBehaviour
-{
-    private float _attackRange = 3f;
-
-    //public abstract bool IsEnemyInRange(Vector3 enemyPosition);
-    public virtual bool IsEnemyInRange(Vector3 enemyPosition)
+    public abstract class Warrior : MonoBehaviour
     {
-        return (transform.position - enemyPosition).magnitude <= _attackRange;
-    }
-}
+        private float _attackRange = 3f;
 
-public class WarriorMelee2 : Warrior
-{
-}
-
-public class WarriorRanged2 : Warrior
-{
-    public override bool IsEnemyInRange(Vector3 enemyPosition)
-    {
-        return base.IsEnemyInRange(enemyPosition);
+        //public abstract bool IsEnemyInRange(Vector3 enemyPosition);
+        public virtual bool IsEnemyInRange(Vector3 enemyPosition)
+        {
+            return (transform.position - enemyPosition).magnitude <= _attackRange;
+        }
     }
 
-    public bool CanAttack(Vector3 enemyPosition) => CanSeeTarget(enemyPosition) && IsEnemyInRange(enemyPosition);
-
-    private bool CanSeeTarget(Vector3 target)
+    public class WarriorMelee2 : Warrior
     {
-        return true;
     }
-}
+
+    public class WarriorRanged2 : Warrior
+    {
+        public override bool IsEnemyInRange(Vector3 enemyPosition)
+        {
+            return base.IsEnemyInRange(enemyPosition);
+        }
+
+        public bool CanAttack(Vector3 enemyPosition) => CanSeeTarget(enemyPosition) && IsEnemyInRange(enemyPosition);
+
+        private bool CanSeeTarget(Vector3 target)
+        {
+            return true;
+        }
+    }
 
 
 // интерфейс, не помогает
 
-public interface IWarrior
-{
-    //private float _attackRange = 3f;
-    public float AttackRange { get; }
-
-    public bool IsEnemyInRange(Vector3 enemyPosition);
-}
-
-public class WarriorMelee3 : MonoBehaviour, IWarrior
-{
-    public float AttackRange => 3f;
-
-    public bool IsEnemyInRange(Vector3 enemyPosition)
+    public interface IWarrior
     {
-        return (transform.position - enemyPosition).magnitude <= AttackRange;
+        //private float _attackRange = 3f;
+        public float AttackRange { get; }
+
+        public bool IsEnemyInRange(Vector3 enemyPosition);
     }
-}
 
-public class WarriorRanged3 : MonoBehaviour, IWarrior
-{
-    // добавим вызов карутины
-    private CoroutineManager _coroutineManager;
-
-    private Animation _animation;
-
-    private IEnumerator PlayDeathAnimation()
+    public class WarriorMelee3 : MonoBehaviour, IWarrior
     {
-        _animation.Play("Death");
-        yield return new WaitUntil(() => !_animation.isPlaying); // ждем пока проиграется анимация
+        public float AttackRange => 3f;
+
+        public bool IsEnemyInRange(Vector3 enemyPosition)
+        {
+            return (transform.position - enemyPosition).magnitude <= AttackRange;
+        }
     }
-    // добавим вызов карутины
 
-
-    // добавим случайно выпадающее золото для примера
-    private List<int> _gold = new() {1, 2, 33, 44, 55, 112};
-
-    public int GetRandomGold()
+    public class WarriorRanged3 : MonoBehaviour, IWarrior
     {
         // добавим вызов карутины
-        // вызывается и без MonoBehaviour
-        _coroutineManager.RunCorounineWithCallback(PlayDeathAnimation(), OnDeath);
+        private CoroutineManager _coroutineManager;
+
+        private Animation _animation;
+
+        private IEnumerator PlayDeathAnimation()
+        {
+            _animation.Play("Death");
+            yield return new WaitUntil(() => !_animation.isPlaying); // ждем пока проиграется анимация
+        }
         // добавим вызов карутины
 
-        int xcount = Random.Range(1, 6);
-        //return _gold[Random.Range(0, _gold.Count)];
-        //используем метод расширения
-        return _gold.GetRandomElementofList();
+
+        // добавим случайно выпадающее золото для примера
+        private List<int> _gold = new() {1, 2, 33, 44, 55, 112};
+
+        public int GetRandomGold()
+        {
+            // добавим вызов карутины
+            // вызывается и без MonoBehaviour
+            _coroutineManager.RunCorounineWithCallback(PlayDeathAnimation(), OnDeath);
+            // добавим вызов карутины
+
+            int xcount = Random.Range(1, 6);
+            //return _gold[Random.Range(0, _gold.Count)];
+            //используем метод расширения
+            return _gold.GetRandomElementofList();
+        }
+
+        // вызов из карутины
+        private void OnDeath()
+        {
+            Debug.Log("Dead");
+        }
+
+        // добавим случайно выпадающее золото для примера
+
+        public float AttackRange => 3f;
+
+        public bool IsEnemyInRange(Vector3 enemyPosition)
+        {
+            return (transform.position - enemyPosition).magnitude <= AttackRange;
+        }
+
+        public bool CanAttack(Vector3 enemyPosition) => CanSeeTarget(enemyPosition) && IsEnemyInRange(enemyPosition);
+
+        private bool CanSeeTarget(Vector3 target)
+        {
+            return true;
+        }
     }
-
-    // вызов из карутины
-    private void OnDeath()
-    {
-        Debug.Log("Dead");
-    }
-
-    // добавим случайно выпадающее золото для примера
-
-    public float AttackRange => 3f;
-
-    public bool IsEnemyInRange(Vector3 enemyPosition)
-    {
-        return (transform.position - enemyPosition).magnitude <= AttackRange;
-    }
-
-    public bool CanAttack(Vector3 enemyPosition) => CanSeeTarget(enemyPosition) && IsEnemyInRange(enemyPosition);
-
-    private bool CanSeeTarget(Vector3 target)
-    {
-        return true;
-    }
-}
 
 
 // статический класс как пример оптимизации методов
 
-public static class UtilsNew
-{
-    // метод расширения который можем использовать в любом месте проекта
-    public static T GetRandomElementofList<T>(this List<T> _list)
+    public static class UtilsNew
     {
-        return _list[Random.Range(0, _list.Count)];
+        // метод расширения который можем использовать в любом месте проекта
+        public static T GetRandomElementofList<T>(this List<T> _list)
+        {
+            return _list[Random.Range(0, _list.Count)];
+        }
     }
-}
 
 
 // карутины
 // кто угодно может обратиться и без MonoBehaviour использовать карутины
-public class CoroutineManager : MonoBehaviour
-{
-    public void RunCorounineWithCallback(IEnumerator coroutine, Action callback)
+    public class CoroutineManager : MonoBehaviour
     {
-        StartCoroutine(RunCoroutine(coroutine, callback));
-    }
+        public void RunCorounineWithCallback(IEnumerator coroutine, Action callback)
+        {
+            StartCoroutine(RunCoroutine(coroutine, callback));
+        }
 
-    private IEnumerator RunCoroutine(IEnumerator coroutine, Action callback)
-    {
-        yield return coroutine;
-        callback?.Invoke();
+        private IEnumerator RunCoroutine(IEnumerator coroutine, Action callback)
+        {
+            yield return coroutine;
+            callback?.Invoke();
+        }
     }
-}
 
 
 // KISS
@@ -191,58 +192,58 @@ public class CoroutineManager : MonoBehaviour
 // менеджер на высоком уровне сам должен понимать что приосходить с его элементами
 
 // KISSExample
-public class Resource
-{
-    public string name;
-    public int amount;
-    private int _cost;
-    public ResourceType resourceType;
-    public float weight;
-    public int maxStackAmount;
-
-    // в зависимости от уровня торговли персонажа и продовца, стоимость меняется
-    // логика лишняя, нужно запрашивать конкретную цену с уровня выше, и затем Trading менеджер должен
-    // нормировать цену с учетом левелов
-    public float GetSellCost(int merchantLevel, int playerLevel)
+    public class Resource
     {
-        return _cost * amount * weight * Math.Max(0, playerLevel - merchantLevel);
+        public string name;
+        public int amount;
+        private int _cost;
+        public ResourceType resourceType;
+        public float weight;
+        public int maxStackAmount;
+
+        // в зависимости от уровня торговли персонажа и продовца, стоимость меняется
+        // логика лишняя, нужно запрашивать конкретную цену с уровня выше, и затем Trading менеджер должен
+        // нормировать цену с учетом левелов
+        public float GetSellCost(int merchantLevel, int playerLevel)
+        {
+            return _cost * amount * weight * Math.Max(0, playerLevel - merchantLevel);
+        }
+
+        // дата создания ресурса лишняя, ресурс не должен знать когда он создал, это должен хранить менеджер инвентаря например
+        // можно добавить поле портится или нет, но не этот метод
+        public DateTime GetCreationTime()
+        {
+            return DateTime.Now;
+        }
     }
 
-    // дата создания ресурса лишняя, ресурс не должен знать когда он создал, это должен хранить менеджер инвентаря например
-    // можно добавить поле портится или нет, но не этот метод
-    public DateTime GetCreationTime()
+    public class ResourceType
     {
-        return DateTime.Now;
     }
-}
-
-public class ResourceType
-{
-}
 
 
 //YAGNI
 //не закладывать логику которая не требуется
-public class Resource2
-{
-    public string name;
-    public int amount;
-    private int _cost;
-    public ResourceType resourceType;
-    private IResourceConverter _converter;
-
-    // а что если на цену будет влиять модификатор конвертации (скидка или что то еще)
-    public float GetCost()
+    public class Resource2
     {
-        return _cost * _converter.ConverterResource();
+        public string name;
+        public int amount;
+        private int _cost;
+        public ResourceType resourceType;
+        private IResourceConverter _converter;
+
+        // а что если на цену будет влиять модификатор конвертации (скидка или что то еще)
+        public float GetCost()
+        {
+            return _cost * _converter.ConverterResource();
+        }
     }
-}
 
 // сделали ненужный интерфейс с базовой реализацией, например возвращает 1f
-public interface IResourceConverter
-{
-    public float ConverterResource();
-}
+    public interface IResourceConverter
+    {
+        public float ConverterResource();
+    }
 
 //Чистый код
 // структура класса
@@ -255,25 +256,29 @@ public interface IResourceConverter
 // - если метод должен что то вернуть GetActionsAmount добавляем Get
 // - например прогресс игрока, если не всегда возвращает результат TryGetArchivementProgress
 
-public class Warrior2 {
-    public int health;
-}
+    public class Warrior2
+    {
+        public int health;
+    }
 
-public class WarriorManager {
-    public Warrior2 EmptyWarrior = new Warrior2{health = 100};
+    public class WarriorManager
+    {
+        public Warrior2 EmptyWarrior = new Warrior2 {health = 100};
 
-    private Dictionary<string, Warrior2> _map = new();
+        private Dictionary<string, Warrior2> _map = new();
 
-    // если не нашли ничего, возвращаем  пустой объект, чтобы не поломать игру
-    public Warrior2 GetWarrior(string id) {
-        if (!_map.ContainsKey(id)) {
-            return EmptyWarrior;
+        // если не нашли ничего, возвращаем  пустой объект, чтобы не поломать игру
+        public Warrior2 GetWarrior(string id)
+        {
+            if (!_map.ContainsKey(id))
+            {
+                return EmptyWarrior;
+            }
+
+            return _map[id];
         }
-
-        return _map[id];
     }
 }
-
 // Порядок:
 // публичные ивенты
 // публичные поля
---
+++ Assets/Scripts/SOLID3.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class SOLID3
+    {
+    }
+
+    // SRP Принцип единой ответственности
+    
+    // как у класса появляется несколько отвественностей
+    // враг поворачивается в сторону игрок и идет к нему - можно объединить в одну логику
+
+    // признаки нарушения
+
+    // - именования класса, если хочется добавить два слова в название для описания логики
+    // создает и перемещает
+    public class SpawnPositioningSystem
+    {
+        public void Spawn()
+        {
+        }
+
+        public void MoveToPosition()
+        {
+        }
+    }
+
+    // объединены предметы для кравта и рецепты
+    public class CraftItemsAndRecipes
+    {
+    }
+
+    // поля которые могут в теории относится к разным логикам
+    public class ShouldBeSeparated
+    {
+        private int _health;
+        private int _mana;
+
+        private int _level;
+        private int _experience;
+
+        private int _killCount;
+
+        private int _damage;
+        private int _resistance;
+    }
+    
+    // пример нарушения SRP
+    // * sealed запрет наследования
+    public sealed class Player : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                this.Move(Vector3.up);
+            }
+            else  if (Input.GetKey(KeyCode.DownArrow))
+            {
+                this.Move(Vector3.down);
+            }
+            else  if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                this.Move(Vector3.left);
+            }
+            else  if (Input.GetKey(KeyCode.RightArrow))
+            {
+                this.Move(Vector3.right);
+            }
+        }
+
+        private void Move(Vector3 direction)
+        {
+            this.transform.position = direction * (Time.deltaTime * _speed);
+            this.transform.rotation = Quaternion.LookRotation(direction,Vector3.up);
+        }
+        
+        // смешивается логика считывания с клавиатуры  Input.GetKey и логика перемещения void Move
+
+    } 
+          
+    // разделение согласно SRP, разделяем на два класса
+    // Player (отвественнен за перемещение) и MoveController (за считывание и передачу вектора)
+    
+    // Input <=> Move
+    
+    //Это позволяет менять метод ввода, ИИ может управлять перемещением,
+    //и наоборот мы можем заставить систему управлять кораблем, врагом...
+    
+    public sealed class Player2 : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+ 
+        public void Move(Vector3 direction)
+        {
+            this.transform.position = direction * _speed;
+            this.transform.rotation = Quaternion.LookRotation(direction,Vector3.up);
+        } 
+    }
+
+    public sealed class MoveController
+    {
+        [SerializeField] private Player2 _player2;
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                this.Move(Vector3.up);
+            }
+            else  if (Input.GetKey(KeyCode.DownArrow))
+            {
+                this.Move(Vector3.down);
+            }
+            else  if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                this.Move(Vector3.left);
+            }
+            else  if (Input.GetKey(KeyCode.RightArrow))
+            {
+                this.Move(Vector3.right);
+            }
+        }
+
+        private void Move(Vector3 direction)
+        {
+            this._player2.Move(direction * Time.deltaTime);
+        }
+
+    }
+    
+    // пример нарушения SRP
+    // смешались логики подсчет очков и выдача достижений 
+    public class ScoreController
+    {
+        private int _points;
+        private bool _notAwardedYet = true;
+
+        public void AddPoints(int amount)
+        {
+            _points += amount;
+            CheckAchievement();
+        }
+
+        private void CheckAchievement()
+        {
+            if (_points > 1000 && _notAwardedYet)
+            {
+                _notAwardedYet = true;
+                Debug.Log("Achievement unlocked");
+            }
+        }
+    }
+    
+    // тут одна ачивка, но их обычно много и у каждой условия, поэтому нужен менеджер
+    
+    // Система очков PointsManager <> Система ачивок AchievementManager <> ачивка Achievement
+    
+    // когда добавляем очки может произойти: обновление UI, возможно выдать ачивку, увеличить уровень перса, и т.д.
+    // поэтому добавляем публичный ивент который будем вызывать каждый раз при добавлении очков
+
+    public class PointsManager
+    {
+        public event Action PointsChangedEvent;
+        public int Points => _points;
+        private int _points;
+
+        public void AddPoints(int points)
+        {
+            _points += points;
+            PointsChangedEvent?.Invoke();
+        }
+    }
+    
+    // для разных ачивок делаем абстрактный класс
+    public abstract class Achievement
+    {
+        public string name;
+        public abstract bool IsCompleted();
+    }
+
+    public class PointsAchievement : Achievement
+    {
+        private PointsManager _pointsManager;
+        public override bool IsCompleted()
+        {
+            return _pointsManager.Points > 1000;
+        }
+    }
+
+    public class AchievementManager
+    {
+        private List<Achievement> _achievements = new();
+        private PointsManager _pointsManager;
+        // чтобы лишний раз не выдавать ачивки
+        private HashSet<string> _completedAchievements = new();
+
+        public void Init()
+        {
+            // подписывается на событие и запускает CheckAchievements
+            _pointsManager.PointsChangedEvent += CheckAchievements;
+            
+        }
+
+        private void CheckAchievements()
+        {
+            foreach (var achievement in _achievements)
+            {
+                if (achievement.IsCompleted() && !_completedAchievements.Contains(achievement.name))
+                {
+                    CompleteAchievement(achievement);
+                }
+            }
+        }
+
+        private void CompleteAchievement(Achievement achievement)
+        {
+            _completedAchievements.Add(achievement.name);
+        }
+    }
+    
+    
+    
+
+    // OCP Принцип открытости закрытости
+    
+    
+}
