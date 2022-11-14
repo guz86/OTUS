33ae2dc6a0a393fd4f2a56666dcefc630b904e64
Pavel Gordienko <guzmo6@rambler.ru>
2022-11-14 06:49:03 +0300

    add TakeDamageMechanics

---
+++ Assets/Scripts/Lessons/Architecture/Mechanics/TakeDamageMechanics.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Lessons.Architecture.Mechanics
+{
+    public class TakeDamageMechanics : MonoBehaviour
+    {
+        [SerializeField] private EventReceiver _takeDamageReceiver;
+        [SerializeField] private IntBehaviour _hitPoints;
+
+        private void OnEnable()
+        {
+            this._takeDamageReceiver.OnEvent += this.OnDamageTaken;
+        }
+
+        private void OnDisable()
+        {
+            this._takeDamageReceiver.OnEvent -= this.OnDamageTaken;
+        }
+
+        private void OnDamageTaken()
+        {
+            const int damage = 1;
+            this._hitPoints.Value -= damage;
+        }
+    }
+}
