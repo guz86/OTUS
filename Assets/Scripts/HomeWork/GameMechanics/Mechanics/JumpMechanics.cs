0b216df3b5245b0d3c75d911d8e895884c58a1bc
Pavel Gordienko <guzmo6@rambler.ru>
2022-11-16 02:57:13 +0300

    test jump

---
+++ Assets/Scripts/HomeWork/GameMechanics/Mechanics/JumpMechanics.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace HomeWork.GameMechanics.Mechanics
+{
+    public class JumpMechanics : MonoBehaviour
+    {
+        [SerializeField] private EventReceiver _eventReceiver;
+        [SerializeField] private GameObject _visualObject;
+        [SerializeField] private float _velocity;
+        [SerializeField] private float _jumpPower;
+        [SerializeField] private bool _isJump;
+
+        private void OnEnable()
+        {
+            _eventReceiver.OnEvent += OnJump;
+        }
+
+        private void OnDisable()
+        {
+            _eventReceiver.OnEvent -= OnJump;
+        }
+
+
+        private void OnJump()
+        {
+            if (!_isJump)
+            {
+                StartCoroutine(JumpRoutine()); 
+                _isJump = false;
+            }
+        }
+
+        private IEnumerator JumpRoutine()
+        {
+            
+            while (_jumpPower <= _velocity && !_isJump)
+            {
+                yield return null;
+                _jumpPower += Time.deltaTime;
+                _visualObject.transform.Translate(new Vector3(0, _jumpPower * 2, 0));
+            }
+
+            _jumpPower = _velocity;
+            
+            _isJump = true;
+            
+            while (_jumpPower >= 0)
+            {
+                yield return null;
+                _jumpPower -= Time.deltaTime;
+
+                    if (_jumpPower < 0) _jumpPower = 0;
+                
+                _visualObject.transform.Translate(new Vector3(0, -_jumpPower * 2, 0));
+            }
+            
+        }
+        
+        
+    }
+}
