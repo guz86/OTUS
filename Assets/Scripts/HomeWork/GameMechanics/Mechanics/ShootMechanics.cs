cbc5937eb6899ef7dd57c8c93d9f017c8c4d0ef0
Pavel Gordienko <guzmo6@rambler.ru>
2022-11-20 17:41:36 +0300

    add ShootBulletEngine for ShootMechanics extract Shot

--- Assets/Scripts/HomeWork/GameMechanics/Mechanics/ShootMechanics.cs
+++ Assets/Scripts/HomeWork/GameMechanics/Mechanics/ShootMechanics.cs
@@ -6,12 +6,9 @@ namespace HomeWork.GameMechanics.Mechanics
     {
         [SerializeField] private EventReceiver _attackReceiver;
         [SerializeField] private TimerBehavior _cooldown;
-        [SerializeField] private GameObject _bulletPrefab;
-        [SerializeField] private GameObject _weaponPoint;
-        [SerializeField] private IntBehaviour __bulletSpeed;
+        [SerializeField] private ShootBulletEngine _shootBulletEngine;
         
-        private GameObject _bulletInstance;
-        
+
         private void OnEnable()
         {
             _attackReceiver.OnEvent += OnShoot;
@@ -30,11 +27,8 @@ namespace HomeWork.GameMechanics.Mechanics
             }
 
             // логика выстрела
-            
-            _bulletInstance = Instantiate(_bulletPrefab,_weaponPoint.transform.position,_weaponPoint.transform.rotation) as GameObject;
-            var bulletInstanceRigidbody = _bulletInstance.GetComponent<Rigidbody>();
-            bulletInstanceRigidbody.AddForce(_weaponPoint.transform.forward * __bulletSpeed.Value);
-            
+            _shootBulletEngine.Shoot();
+
             // сбросить и запустить таймер снова
             _cooldown.ResetTime();
             _cooldown.Play();
