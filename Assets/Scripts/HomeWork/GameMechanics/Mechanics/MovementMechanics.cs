fec7bd0361081cdcb7ce51c11a253567d677fea4
Pavel Gordienko <guzmo6@rambler.ru>
2022-11-20 16:35:56 +0300

    fix division by zero

--- Assets/Scripts/HomeWork/GameMechanics/Mechanics/MovementMechanics.cs
+++ Assets/Scripts/HomeWork/GameMechanics/Mechanics/MovementMechanics.cs
@@ -27,22 +27,27 @@ namespace HomeWork.GameMechanics.Mechanics
 
         private IEnumerator Move(Vector3 targetPosition)
         {
+            
             var startPosition = _visualObject.transform.position;
 
+            // если позиция не меняется
+            if (targetPosition - startPosition == Vector3.zero) yield return null;
+            
             float progress = 0;
-            float time = 0;
-            var distance = (startPosition - targetPosition).magnitude;
+            float currentTime = 0;
+            var distance = (targetPosition - startPosition).magnitude;
 
             while (progress < 1)
             {
-                time += Time.deltaTime;
-                progress = time / distance * _MovementSpeed.Value;
+                yield return null;
+                currentTime += Time.deltaTime;
+                progress = currentTime * _MovementSpeed.Value / distance;
                 var targetX = targetPosition.x - startPosition.x;
                 var targetZ = targetPosition.z - startPosition.z;
                 var target = new Vector3((targetX) * progress, 0f, (targetZ) * progress);
 
                 _visualObject.transform.position = target + startPosition;
-                yield return null;
+                
             }
 
             _coroutineMove = null;
