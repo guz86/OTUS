4136ea646ed4545c6a84b84348783f30d6efed4b
Pavel Gordienko <guzmo6@rambler.ru>
2022-11-21 13:20:32 +0300

    add Character FACADE

--- Assets/Scripts/GameobjectsComponents5.cs
+++ Assets/Scripts/GameobjectsComponents5.cs
@@ -3,5 +3,11 @@ namespace Lessons
     public class GameobjectsComponents5
     {
     }
+    // когда подходит компонентный подход и когда нет и нужно ООП
+    // ООП
+    //  напишем фасад (англ. Facade) для Character (интерфейс к сложной системе классов) еще есть Enemy
+    
+    
+    
     
 }
