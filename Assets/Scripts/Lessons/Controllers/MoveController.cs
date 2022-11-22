04c5e100d8dd8b14de98cc93a83021c29d3a1a03
Pavel Gordienko <guzmo6@rambler.ru>
2022-11-22 18:15:14 +0300

    add MoveController cache

--- Assets/Scripts/Lessons/Controllers/MoveController.cs
+++ Assets/Scripts/Lessons/Controllers/MoveController.cs
@@ -5,12 +5,19 @@ namespace Lessons.Architecture.Mechanics
     public class MoveController : AbstractMoveController
     {
         [SerializeField] private Entity _unit;
-        
+
+        private IMoveComponent _moveComponent;
+
+        private void Awake()
+        {
+            _moveComponent = _unit.Get<IMoveComponent>();
+        }
+
         protected override void Move(Vector3 direction)
         {
             const float speed = 5.0f;
             var velocity = direction * (speed * Time.deltaTime);
-            _unit.Get<IMoveComponent>().Move(velocity);
+            _moveComponent.Move(velocity);
         }
     }
 }
